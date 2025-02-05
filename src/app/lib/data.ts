29bd74c4ca3706867b35ed5048e0ffe6aca956b7
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-02-05 17:27:16 +1100

    feat: fetch selected project data

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,6 +1,7 @@
 import { sql } from '@vercel/postgres';
 import {
     User,
+    Project
 } from './definitions';
 
 export async function fetchUser(){
@@ -27,3 +28,30 @@ export async function fetchUser(){
       throw new Error('Failed to fetch user.');
     }
 }
+
+export async function fetchSelectedWork(): Promise<Project[]> {
+  try {
+    const projects = await sql<Project>`
+      SELECT 
+        p.id, 
+        p.title, 
+        p.description,
+        ARRAY_AGG(DISTINCT pt.tool) AS tools,
+        ARRAY_AGG(DISTINCT pc.category) AS categories,
+        ARRAY_AGG(DISTINCT pl.link) AS links
+      FROM projects p
+      LEFT JOIN project_tools pt ON p.id = pt.project_id
+      LEFT JOIN project_categories pc ON p.id = pc.project_id
+      LEFT JOIN project_links pl ON p.id = pl.project_id
+      GROUP BY p.id, p.title, p.description
+      ORDER BY p.date DESC
+      LIMIT 5;
+    `;
+
+    return projects.rows;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to fetch selected work.');
+  }
+}
+
