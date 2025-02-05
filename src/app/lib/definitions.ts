d1830fb96f35e1ec112f6ca580903fefb9e8b41c
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-02-05 17:27:48 +1100

    fix: updates definition

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -11,16 +11,25 @@ export type User = {
   currentlyReading: string;
 };
 
+// export type Project = {
+//   id: string;
+//   title: string;
+//   description: string;
+//   links: string[];
+//   tools: string[];
+//   heroImage: string;
+//   categories: string[];
+//   text: string;
+//   role: string;
+//   date: string;
+//   projectImages: string[];
+// };
+
 export type Project = {
   id: string;
   title: string;
   description: string;
-  links: string[];
   tools: string[];
-  heroImage: string;
   categories: string[];
-  text: string;
-  role: string;
-  date: string;
-  projectImages: string[];
+  links: string[];
 };
