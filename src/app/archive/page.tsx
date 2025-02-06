9097442f7662b39e9d9751aa37b0b000f4812906
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-02-06 18:58:15 +1100

    feat: fetch archive page projects

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -3,23 +3,17 @@ import { fetchAllProjects } from '@/app/lib/data';
 export default async function WorkPage() {
   const projects = await fetchAllProjects();
 
-  const selectedProjectIds = [
-    "acd50b8c-9e99-4fb5-b388-b5ed5aa80d20",
-    "7188a7c6-4d17-45f7-9469-ca5f31f9904c"
-  ]; 
-
-  const selectedProjects = projects.filter((p) => selectedProjectIds.includes(p.id));
 
   return (
     <main>
       <h1>Selected Work</h1>
 
       <div>
-        {selectedProjects.length > 0 ? (
-          selectedProjects.map((project) => (
+        {projects.length > 0 ? (
+          projects.map((project) => (
             <div key={project.id}>
               <h2>{project.title}</h2>
-              <p>{project.description}</p>
+              <p>{project.date}</p>
 
               <div>
                 {project.links.map((link, index) => (
