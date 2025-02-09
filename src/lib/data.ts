3f665d2ebb4b9b1505bf76933a6d9b316e747406
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-02-09 19:49:30 +1100

    fix: mismatch in project type

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -60,4 +60,38 @@ export async function fetchAllProjects(): Promise<Project[]> {
   }
 }
 
+export async function fetchProjectById(id: string[]): Promise<Project[]> {
+  if (id.length === 0) return [];
+  try {
+    const query = `
+      SELECT 
+        p.id, 
+        p.title, 
+        p.description,
+        p.text,
+        p.heroimage AS "heroImage",
+        p.role,
+        p.date,
+        ARRAY_AGG(DISTINCT pc.category) AS categories,
+        ARRAY_AGG(DISTINCT pt.tool) AS tools,
+        ARRAY_AGG(DISTINCT pl.link) AS links,
+        ARRAY_AGG(DISTINCT pi.image) AS images
+      FROM projects p
+      LEFT JOIN project_categories pc ON p.id = pc.project_id
+      LEFT JOIN project_tools pt ON p.id = pt.project_id
+      LEFT JOIN project_links pl ON p.id = pl.project_id
+      LEFT JOIN project_images pi ON p.id = pi.project_id
+      WHERE p.id = ANY($1::uuid[])
+      GROUP BY p.id, p.title, p.description, p.text, p.heroimage, p.role, p.date
+      ORDER BY p.date DESC;
+    `;
+
+    const projects = await sql.query(query, [id]);
+    return projects.rows;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to fetch all projects.');
+  }
+}
+
 
