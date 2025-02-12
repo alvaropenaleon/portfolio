2bb060ae055d34e161d64beefa96f3efecb61b18
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-02-12 17:32:58 +1100

    feat: incorporates sidebar layout into archive page

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,13 +1,25 @@
 import { fetchAllProjects } from '@/lib/data';
 import ArchiveList from '@/components/archive/archiveList';
+import SidebarLayout from '@/components/ui/sidebarLayout';
 
 export default async function ArchivePage() {
   const projects = await fetchAllProjects();
 
+  if (!projects) {
+    return <p className="text-red-500">Project data not found.</p>;
+  }
+
+  // Define sidebar content
+  const sidebarContent = (
+    <>
+      <p>Category:</p>
+    </>
+  );
+
   return (
-    <main className="p-6">
-      <h1 className="text-3xl font-bold">Archive</h1>
+    <SidebarLayout sidebar={sidebarContent}>
+      <h1>Archive</h1>
       <ArchiveList projects={projects} />
-    </main>
+    </SidebarLayout>
   );
 }
