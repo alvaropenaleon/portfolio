bd338358485605d5d6191980c6871b5c472f7229
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-02-26 17:30:47 +1100

    feat: incorporates menu in layout

--- src/components/ui/sidebarLayout.tsx
+++ src/components/ui/sidebarLayout.tsx
@@ -1,5 +1,6 @@
 // components/SidebarLayout.tsx
 import styles from '@/styles/ui/layout.module.css';
+import MenuBar from '@/components/ui/menu'
 
 interface SidebarLayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ interface SidebarLayoutProps {
 export default function SidebarLayout({ children, sidebar }: SidebarLayoutProps) {
   return (
     <div className={sidebar ? styles.withSidebar : styles.fullWidth}>
+      <MenuBar />
       {sidebar && (
         <div className={styles.sidebar}>
           <div className={styles.sidebarBottom}>
