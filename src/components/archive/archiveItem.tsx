653394d044bda3b1770a7f982359c2d124e95ce6
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-03-11 19:13:43 +1100

    fix: merges title and category single row

--- src/components/archive/archiveItem.tsx
+++ src/components/archive/archiveItem.tsx
@@ -2,38 +2,38 @@ import { Project } from '@/lib/definitions';
 import styles from '@/styles/ui/row.module.css';
 
 type ArchiveListProps = {
-  project: Project;
+    project: Project;
 };
 
 export default function ArchiveItem({ project }: ArchiveListProps) {
-  return (
-    <div className={styles.row5col}>
-      <p className={styles.col1}>{project.date}</p>
+    return (
+        <div className={styles.row4col}>
+            <p className={styles.col1}>{project.date}</p>
 
-      {/* Categories */}
-      <div className={styles.col2}>
-        {project.categories.map((category, index) => (
-          <p key={index}>{category}</p>
-        ))}
-      </div>
+            {/* Categories */}
+            <div className={styles.col2}>
+                {project.title}
+                <br />
+                {project.categories.map((category, index) => (
+                    <p key={index}>{category}</p>
+                ))}
+            </div>
 
-      <p className={styles.col3}>{project.title}</p>
+            {/* Tools */}
+            <div className={styles.col3}>
+                {project.tools.map((tool, index) => (
+                    <p key={index}>{tool}</p>
+                ))}
+            </div>
 
-      {/* Tools */}
-      <div className={styles.col4}>
-        {project.tools.map((tool, index) => (
-          <p key={index}>{tool}</p>
-        ))}
-      </div>
-
-      {/* Links */}
-      <div className={styles.col5}>
-        {project.links.map(({ url, type }, index) => (
-          <a key={index} href={url} target="_blank" rel="noopener noreferrer">
-            {type === 'code' ? 'Code' : 'Demo'}
-          </a>
-        ))}
-      </div>
-    </div>
-  );
+            {/* Links */}
+            <div className={styles.col4}>
+                {project.links.map(({ url, type }, index) => (
+                    <a key={index} href={url} target="_blank" rel="noopener noreferrer">
+                        {type === 'code' ? 'Code' : 'Demo'}
+                    </a>
+                ))}
+            </div>
+        </div>
+    );
 }
