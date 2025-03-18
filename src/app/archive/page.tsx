37c3c5ed878fa6e9a9c793b6e27deead91a36f6e
alvaropenaleon <s3909789@student.rmit.edu.au>
2025-03-18 18:34:51 +1100

    feat: creates sticky anchor tags for archive page

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,12 +1,9 @@
-// app/archive/page.tsx
-
 import { fetchFilteredProjects, fetchUser,  ITEMS_PER_PAGE } from '@/lib/data';
 import ArchiveList from '@/components/archive/archiveList';
 import SidebarLayout from '@/components/ui/sidebarLayout';
 import Search from '@/components/archive/search';
 import Pagination from '@/components/archive/pagination';
 
-// 1) Define searchParams as a Promise
 interface ArchivePageProps {
   searchParams: Promise<{
     query?: string;
@@ -15,20 +12,19 @@ interface ArchivePageProps {
 }
 
 export default async function ArchivePage({ searchParams }: ArchivePageProps) {
-  // 2) Await searchParams
   const sp = await searchParams;
 
-  // 3) Now read query/page from the awaited object
+  // Read query/page
   const query = sp.query ?? '';
   const currentPage = Number(sp.page) || 1;
 
-  // Fetch user + filtered projects
   const user = await fetchUser();
   const { projects, totalPages } = await fetchFilteredProjects(query, currentPage,  ITEMS_PER_PAGE);
 
-  // Sidebar, etc.
-  const sidebarContent = <p>Category:</p>;
-
+  const sidebarContent = (
+    <></>
+  );
+  
   return (
     <SidebarLayout sidebar={sidebarContent} user={user}>
       <Search placeholder="Search projects..." />
--- src/components/archive/archiveList.tsx
+++ src/components/archive/archiveList.tsx
@@ -1,5 +1,9 @@
 import ArchiveItem from '@/components/archive/archiveItem';
 import { Project } from '@/lib/definitions';
+import styles from '@/styles/ui/row.module.css';
+import layoutStyles from '@/styles/archive/archiveList.module.css'; 
+
+
 
 type ArchiveListProps = {
   projects: Project[];
@@ -8,6 +12,17 @@ type ArchiveListProps = {
 export default function ArchiveList({ projects }: ArchiveListProps) {
   return (
     <div>
+      {/* header */}
+      <div className={layoutStyles.stickyHeader}>
+        <div className={styles.row4col}>
+          <p className={styles.col1}>Date</p>
+          <p className={styles.col2}>Project</p>
+          <p className={styles.col3}>Skillset</p>
+          <p className={styles.col4}>Link</p>
+        </div>
+      </div>
+
+      {/* list items */}
       {projects.map((project) => (
         <ArchiveItem key={project.id} project={project} />
       ))}
